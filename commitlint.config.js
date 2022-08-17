f6306ef68a555c3ab7828366dc9287e94b2933d9
happy.yin <happy.yin@quectel.com>
2022-08-17 16:01:18 +0800

    feat(project): 前端工程架构初始化

---
+++ commitlint.config.js
@@ -0,0 +1,4 @@
+// https://www.conventionalcommits.org/zh-hans/v1.0.0-beta.4/#%e7%ba%a6%e5%ae%9a%e5%bc%8f%e6%8f%90%e4%ba%a4%e8%a7%84%e8%8c%83
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+}
